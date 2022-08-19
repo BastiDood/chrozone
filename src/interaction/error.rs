bcd41c95e2bf286781c90ac2dcb9e481564e469e
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-08-19 19:55:53 +0800

    Feat: prepare scaffolding for autocompletion

--- src/interaction/error.rs
+++ src/interaction/error.rs
@@ -3,12 +3,16 @@ use core::fmt;
 pub type Result<T> = core::result::Result<T, Error>;
 
 pub enum Error {
+    Fatal,
+    MissingPayload,
     UnsupportedInteractionType,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(match self {
+            Self::Fatal => "Unrecoverable error. This is unexpected behavior. Please file a bug report.",
+            Self::MissingPayload => "No interaction data present.",
             Self::UnsupportedInteractionType => "Unsupported interaction type.",
         })
     }
--- src/interaction/mod.rs
+++ src/interaction/mod.rs
@@ -1,20 +1,40 @@
 mod error;
 
-use twilight_model::{application::interaction::Interaction, http::interaction::InteractionResponse};
+use twilight_model::{
+    application::interaction::{application_command::CommandData, Interaction},
+    http::interaction::InteractionResponse,
+};
 
-pub fn try_respond(interaction: Interaction) -> error::Result<InteractionResponse> {
+fn on_app_command(data: CommandData) -> error::Result<InteractionResponse> {
+    todo!()
+}
+
+fn on_autocomplete(data: CommandData) -> InteractionResponse {
+    todo!()
+}
+
+fn try_respond(interaction: Interaction) -> error::Result<InteractionResponse> {
     use twilight_model::{
-        application::interaction::InteractionType::{ApplicationCommand, Ping},
+        application::interaction::{
+            InteractionData,
+            InteractionType::{ApplicationCommand, ApplicationCommandAutocomplete, Ping},
+        },
         http::interaction::InteractionResponseType::Pong,
     };
 
-    match interaction.kind {
+    let is_comm = match interaction.kind {
+        ApplicationCommand => true,
+        ApplicationCommandAutocomplete => false,
         Ping => return Ok(InteractionResponse { kind: Pong, data: None }),
-        ApplicationCommand => (),
         _ => return Err(error::Error::UnsupportedInteractionType),
-    }
+    };
 
-    todo!()
+    let data = match interaction.data.ok_or(error::Error::MissingPayload)? {
+        InteractionData::ApplicationCommand(data) => *data,
+        _ => return Err(error::Error::Fatal),
+    };
+
+    Ok(if is_comm { on_app_command(data)? } else { on_autocomplete(data) })
 }
 
 pub fn respond(interaction: Interaction) -> InteractionResponse {
