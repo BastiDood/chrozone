d8ee8361ebca615e15690760da537f881d16e487
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-08-20 13:41:09 +0800

    Feat: use Sublime fuzzy search algorithm

--- src/util.rs
+++ src/util.rs
@@ -5,12 +5,9 @@
 /// # Panic
 /// Panics if `count` is greater than or equal to the length of [`TZ_VARIANTS`](chrono_tz::TZ_VARIANTS).
 pub fn autocomplete_tz(query: &str, count: usize) -> alloc::vec::Vec<&'static str> {
+    use core::cmp::Reverse;
     let mut names = chrono_tz::TZ_VARIANTS.map(chrono_tz::Tz::name).to_vec();
-    names.select_nth_unstable_by(count, move |&a, &b| {
-        let first = strsim::sorensen_dice(query, a);
-        let second = strsim::sorensen_dice(query, b);
-        second.total_cmp(&first)
-    });
+    names.select_nth_unstable_by_key(count, move |&target| Reverse(sublime_fuzzy::best_match(query, target)));
     names
 }
 
@@ -19,6 +16,6 @@ mod tests {
     #[test]
     fn autocomplete_queries() {
         let names = super::autocomplete_tz("Asia/Ma", 5);
-        assert_eq!(&names[..5], &["Asia/Macao", "Asia/Manila", "Asia/Magadan", "Asia/Macau", "Asia/Makassar"]);
+        assert_eq!(&names[..5], &["Asia/Manila", "Asia/Macao", "Asia/Macau", "Asia/Magadan", "Asia/Makassar"]);
     }
 }
