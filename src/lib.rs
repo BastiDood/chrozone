09cdaf9749b2e28683972149ac3da61bd89090a7
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-10-10 19:51:53 +0800

    feat: add health check path

--- src/lib.rs
+++ src/lib.rs
@@ -23,45 +23,57 @@ pub async fn try_respond<B>(
 where
     B: AsRef<[u8]>,
 {
-    if method != Method::POST {
-        log::error!("Rejected non-POST request: {method} {path}");
-        return Err(StatusCode::METHOD_NOT_ALLOWED);
+    match method {
+        Method::GET => {
+            if path == "/" {
+                log::info!("Health check pinged!");
+                Ok(Response::new(Body::empty()))
+            } else {
+                log::error!("Invalid health check path: {path}");
+                Err(StatusCode::NOT_FOUND)
+            }
+        }
+        Method::POST => {
+            if path != "/discord" {
+                log::error!("Rejected invalid path request: {path}");
+                return Err(StatusCode::NOT_FOUND);
+            }
+
+            // Retrieve security headers
+            let maybe_sig = headers.get("X-Signature-Ed25519");
+            let maybe_time = headers.get("X-Signature-Timestamp");
+            let (sig, timestamp) = maybe_sig.zip(maybe_time).ok_or(StatusCode::UNAUTHORIZED)?;
+            let signature = hex::decode(sig).map_err(|_| StatusCode::BAD_REQUEST)?;
+            log::debug!("Timestamp and signature retrieved.");
+
+            // Append body after the timestamp
+            let payload = hyper::body::to_bytes(body).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+            let mut message = timestamp.as_bytes().to_vec();
+            message.extend_from_slice(&payload);
+            log::debug!("Fully received payload body.");
+
+            // Validate the challenge
+            pub_key.verify(&message, &signature).map_err(|_| StatusCode::UNAUTHORIZED)?;
+            drop(message);
+            drop(signature);
+            log::debug!("Ed25519 signature verified.");
+
+            // Parse incoming interaction
+            let interaction = serde_json::from_slice(&payload).map_err(|_| StatusCode::BAD_REQUEST)?;
+            drop(payload);
+            log::debug!("Interaction JSON body parsed.");
+
+            let reply = interaction::respond(interaction);
+            let body = serde_json::to_string(&reply).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?.into();
+
+            use hyper::header::{HeaderValue, CONTENT_TYPE};
+            let mut res = Response::new(body);
+            res.headers_mut().append(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+            Ok(res)
+        }
+        _ => {
+            log::error!("Rejected non-POST request: {method} {path}");
+            Err(StatusCode::METHOD_NOT_ALLOWED)
+        }
     }
-
-    if path != "/discord" {
-        log::error!("Rejected invalid path request: {path}");
-        return Err(StatusCode::NOT_FOUND);
-    }
-
-    // Retrieve security headers
-    let maybe_sig = headers.get("X-Signature-Ed25519");
-    let maybe_time = headers.get("X-Signature-Timestamp");
-    let (sig, timestamp) = maybe_sig.zip(maybe_time).ok_or(StatusCode::UNAUTHORIZED)?;
-    let signature = hex::decode(sig).map_err(|_| StatusCode::BAD_REQUEST)?;
-    log::debug!("Timestamp and signature retrieved.");
-
-    // Append body after the timestamp
-    let payload = hyper::body::to_bytes(body).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let mut message = timestamp.as_bytes().to_vec();
-    message.extend_from_slice(&payload);
-    log::debug!("Fully received payload body.");
-
-    // Validate the challenge
-    pub_key.verify(&message, &signature).map_err(|_| StatusCode::UNAUTHORIZED)?;
-    drop(message);
-    drop(signature);
-    log::debug!("Ed25519 signature verified.");
-
-    // Parse incoming interaction
-    let interaction = serde_json::from_slice(&payload).map_err(|_| StatusCode::BAD_REQUEST)?;
-    drop(payload);
-    log::debug!("Interaction JSON body parsed.");
-
-    let reply = interaction::respond(interaction);
-    let body = serde_json::to_string(&reply).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?.into();
-
-    use hyper::header::{HeaderValue, CONTENT_TYPE};
-    let mut res = Response::new(body);
-    res.headers_mut().append(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-    Ok(res)
 }
