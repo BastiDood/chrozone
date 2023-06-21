541b6d984f680dacb0ea75d42a4b43af3078e54d
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-06-22 01:30:02 +0800

    refactor: prefer `textdistance` crate over `strsim`

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,21 +2,6 @@ pub mod float;
 pub mod sort;
 
 use alloc::vec::Vec;
-use core::{char::ToLowercase, iter::FlatMap, str::Chars};
-
-struct CharWrapper<'a>(&'a str);
-
-impl<'a> IntoIterator for &'a CharWrapper<'a> {
-    type IntoIter = FlatMap<Chars<'a>, ToLowercase, fn(char) -> ToLowercase>;
-    type Item = char;
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.chars().flat_map(char::to_lowercase)
-    }
-}
-
-fn compute_score(a: &str, b: &str) -> f64 {
-    strsim::generic_jaro_winkler(&CharWrapper(a), &CharWrapper(b))
-}
 
 /// Compares a `query` string to a list of supported IANA timezones. The return value
 /// is a vector of string slices, where the first `count` elements are an (unspecified)
@@ -32,7 +17,7 @@ pub fn autocomplete_tz(query: &str, count: usize) -> Vec<&'static str> {
     let mut cache = hashbrown::HashMap::with_capacity(32);
 
     sort::top_n_by_key(&mut names, count, |&tz| {
-        let score = *cache.entry(tz).or_insert_with(|| compute_score(query, tz));
+        let score = *cache.entry(tz).or_insert_with(|| textdistance::str::jaro_winkler(tz, query));
         Reverse(TotalDouble(score))
     });
 
@@ -44,6 +29,9 @@ mod tests {
     #[test]
     fn autocomplete_queries() {
         let names = super::autocomplete_tz("Asia/Ma", 5);
-        assert_eq!(&names[..5], &["Asia/Macao", "Asia/Macau", "Asia/Manila", "Asia/Magadan", "Asia/Makassar"]);
+        assert_eq!(&names[..5], &["Asia/Macao", "Asia/Macau", "Asia/Muscat", "Asia/Manila", "Asia/Magadan"]);
+
+        let names = super::autocomplete_tz("Asia/Man", 5);
+        assert_eq!(&names[..5], &["Asia/Manila", "Asia/Magadan", "Asia/Amman", "Asia/Macao", "Asia/Macau"]);
     }
 }
